62a5fa19c7155c5ceda407e240ed8bb8c3323943
Mazuros2 <s24440@pjwstk.edu.pl>
2024-08-20 12:34:07 +0200

    Project Innit

---
+++ Backend/projekt_inzynierski/src/test/java/dev/projekt_inzynierski/ProjektInzynierskiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projekt_inzynierski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektInzynierskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
