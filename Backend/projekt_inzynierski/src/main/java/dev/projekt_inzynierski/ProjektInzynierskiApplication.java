889e67f62f7fb55fc147eeac7755f197b583a221
Mazuros2 <s24440@pjwstk.edu.pl>
2024-09-07 11:51:11 +0200

    Dodałem 8 klubów z pierszej Ligii i dodałem jeszcze 3 kluby do Ekstraklasy - 11:51

--- Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/ProjektInzynierskiApplication.java
+++ Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/ProjektInzynierskiApplication.java
@@ -75,12 +75,94 @@ public class ProjektInzynierskiApplication implements CommandLineRunner {
 				.liga(Ekstraklasa)
 				.build();
 
+		Klub ZaglebieLubin = Klub.builder()
+				.nazwa_klubu("KGHM Zagłębie Lubin")
+				.rok_zalozenia(LocalDate.of(1946,1,1))
+				.liga(Ekstraklasa)
+				.build();
+
+
+		Klub SlaskWroclaw = Klub.builder()
+				.nazwa_klubu("Slask Wroclaw")
+				.rok_zalozenia(LocalDate.of(1947,1,1))
+				.liga(Ekstraklasa)
+				.build();
+
+		Klub RadomiakRadom = Klub.builder()
+				.nazwa_klubu("Radomiak Radom")
+				.rok_zalozenia(LocalDate.of(1910,1,1))
+				.liga(Ekstraklasa)
+				.build();
 
 		klubRepository.save(LegiaWarszawa);
 		klubRepository.save(LechPoznan);
 		klubRepository.save(GKSKatowice);
 		klubRepository.save(GornikZabrze);
 		klubRepository.save(RakowCzestochowa);
+		klubRepository.save(ZaglebieLubin);
+		klubRepository.save(SlaskWroclaw);
+		klubRepository.save(RadomiakRadom);
+
+		//Kluby I Ligii
+		Klub RuchChorzow = Klub.builder()
+				.nazwa_klubu("Ruch Chorzow")
+				.rok_zalozenia(LocalDate.of(1920,1,1))
+				.liga(ILiga)
+				.build();
+
+		Klub ZniczPruszkow = Klub.builder()
+				.nazwa_klubu("Znicz Pruszkow")
+				.rok_zalozenia(LocalDate.of(1922,1,1))
+				.liga(ILiga)
+				.build();
+
+		Klub PogonSiedlce = Klub.builder()
+				.nazwa_klubu("Pogon Siedlce")
+				.rok_zalozenia(LocalDate.of(1944,1,1))
+				.liga(ILiga)
+				.build();
+
+
+		Klub GKSTychy = Klub.builder()
+				.nazwa_klubu("GKS Tychy")
+				.rok_zalozenia(LocalDate.of(1971,1,1))
+				.liga(ILiga)
+				.build();
+
+		Klub BrukBetTermalica = Klub.builder()
+				.nazwa_klubu("Bruk-Bet Termalica")
+				.rok_zalozenia(LocalDate.of(1922,1,1))
+				.liga(ILiga)
+				.build();
+
+		Klub ArkaGdynia = Klub.builder()
+				.nazwa_klubu("Arka Gdynia")
+				.rok_zalozenia(LocalDate.of(1929,1,1))
+				.liga(ILiga)
+				.build();
+
+		Klub MiedzLegnica = Klub.builder()
+				.nazwa_klubu("Miedz Legnica")
+				.rok_zalozenia(LocalDate.of(1971,1,1))
+				.liga(ILiga)
+				.build();
+
+		Klub GornikLeczna = Klub.builder()
+				.nazwa_klubu("Gornik Leczna")
+				.rok_zalozenia(LocalDate.of(1979,1,1))
+				.liga(ILiga)
+				.build();
+
+
+		klubRepository.save(RuchChorzow);
+		klubRepository.save(ZniczPruszkow);
+		klubRepository.save(PogonSiedlce);
+		klubRepository.save(GKSTychy);
+		klubRepository.save(BrukBetTermalica);
+		klubRepository.save(ArkaGdynia);
+		klubRepository.save(MiedzLegnica);
+		klubRepository.save(GornikLeczna);
+
 		List<Klub> kluby = klubRepository.findAll();
 		System.out.println("Lista klubow:");
 		for (Klub klub : kluby) {
