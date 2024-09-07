6dab493024220fda2288d555a37a398d433ef087
Mazuros2 <s24440@pjwstk.edu.pl>
2024-09-07 10:15:53 +0200

    Stworzyłem Ekstraklase i I Lige - 10:15

--- Backend/projekt_inzynierski/src/main/java/AppRunner.java
+++ Backend/projekt_inzynierski/src/main/java/AppRunner.java
@@ -1,3 +1,5 @@
+import dev.projekt_inzynierski.models.Liga;
+import dev.projekt_inzynierski.repository.Klub.LigaRepository;
 import lombok.RequiredArgsConstructor;
 import dev.projekt_inzynierski.models.Klub;
 import org.springframework.boot.CommandLineRunner;
@@ -9,11 +11,29 @@ import dev.projekt_inzynierski.repository.Klub.KlubRepository;
 public class AppRunner implements CommandLineRunner {
 
     private final KlubRepository klubRepository;
+    private final LigaRepository ligaRepository;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println(" ");
         klubRepository.findAll();
         klubRepository.save(Klub.builder().nazwa_klubu("xd").build());
+
+        Liga Ekstraklasa = Liga.builder()
+                .nazwa_Ligi("Ekstraklasa")
+                .poziom_Ligi(1)
+                .build();
+
+        Liga ILiga = Liga.builder()
+                .nazwa_Ligi("I Liga")
+                .poziom_Ligi(2)
+                .build();
+
+
+        ligaRepository.save(Ekstraklasa);
+        ligaRepository.save(ILiga);
+
+
+
     }
 }
