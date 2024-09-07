05bf6df9e66acda68eb1acff428f41332b111171
Mazuros2 <s24440@pjwstk.edu.pl>
2024-09-07 10:23:53 +0200

    Stworzyłem 5 klubów w Ekstraklasie (Legia, Lech, GKS, Górnik, Raków) - 10:23

--- Backend/projekt_inzynierski/src/main/java/AppRunner.java
+++ Backend/projekt_inzynierski/src/main/java/AppRunner.java
@@ -3,8 +3,11 @@ import dev.projekt_inzynierski.repository.Klub.LigaRepository;
 import lombok.RequiredArgsConstructor;
 import dev.projekt_inzynierski.models.Klub;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Component;
 import dev.projekt_inzynierski.repository.Klub.KlubRepository;
+
+import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @Component
@@ -33,6 +36,43 @@ public class AppRunner implements CommandLineRunner {
         ligaRepository.save(Ekstraklasa);
         ligaRepository.save(ILiga);
 
+        Klub LegiaWarszawa = Klub.builder()
+                .nazwa_klubu("Legia Warszawa")
+                .rok_zalozenia(LocalDate.of(1916,1,1))
+                .liga(Ekstraklasa)
+                .build();
+
+        Klub LechPoznan = Klub.builder()
+                .nazwa_klubu("Lech Poznań")
+                .rok_zalozenia(LocalDate.of(1922,1,1))
+                .liga(Ekstraklasa)
+                .build();
+
+        Klub GKSKatowice = Klub.builder()
+                .nazwa_klubu("GKS Katowice")
+                .rok_zalozenia(LocalDate.of(1964,1,1))
+                .liga(Ekstraklasa)
+                .build();
+
+
+        Klub GornikZabrze = Klub.builder()
+                        .nazwa_klubu("Górnik Zabrze")
+                        .rok_zalozenia(LocalDate.of(1948,1,1))
+                        .liga(Ekstraklasa)
+                        .build();
+
+        Klub RakowCzestochowa = Klub.builder()
+                        .nazwa_klubu("Raków Częstochowa")
+                        .rok_zalozenia(LocalDate.of(1921,1,1))
+                        .liga(Ekstraklasa)
+                        .build();
+
+
+        klubRepository.save(LegiaWarszawa);
+        klubRepository.save(LechPoznan);
+        klubRepository.save(GKSKatowice);
+        klubRepository.save(GornikZabrze);
+        klubRepository.save(RakowCzestochowa);
 
 
     }
