fedefec0259a453cf3c2377593b478fe93693140
Mazuros2 <s24440@pjwstk.edu.pl>
2024-09-07 15:35:48 +0200

    Dodałem po 4 trofea dla Legii i Górnika, dodałem też array liste do każdego klubu żeby można było dodawać trofea bo wyrzucało wcześniej nullpointer exception - 15:35

--- Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/ProjektInzynierskiApplication.java
+++ Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/ProjektInzynierskiApplication.java
@@ -2,14 +2,17 @@ package dev.projekt_inzynierski;
 
 import dev.projekt_inzynierski.models.Klub;
 import dev.projekt_inzynierski.models.Liga;
+import dev.projekt_inzynierski.models.Trofeum;
 import dev.projekt_inzynierski.repository.Klub.KlubRepository;
 import dev.projekt_inzynierski.repository.Klub.LigaRepository;
+import dev.projekt_inzynierski.repository.Klub.TrofeumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -20,6 +23,9 @@ public class ProjektInzynierskiApplication implements CommandLineRunner {
 
 	@Autowired
 	private KlubRepository klubRepository;
+
+	@Autowired
+	private TrofeumRepository trofeumRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjektInzynierskiApplication.class, args);
@@ -52,52 +58,60 @@ public class ProjektInzynierskiApplication implements CommandLineRunner {
 		//Kluby Ekstraklasy
 		Klub LegiaWarszawa = Klub.builder()
 				.nazwa_klubu("Legia Warszawa")
-				.rok_zalozenia(LocalDate.of(1916,1,1))
+				.rok_zalozenia(LocalDate.of(1916, 1, 1))
 				.liga(Ekstraklasa)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub LechPoznan = Klub.builder()
 				.nazwa_klubu("Lech Poznan")
-				.rok_zalozenia(LocalDate.of(1922,1,1))
+				.rok_zalozenia(LocalDate.of(1922, 1, 1))
 				.liga(Ekstraklasa)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub GKSKatowice = Klub.builder()
 				.nazwa_klubu("GKS Katowice")
-				.rok_zalozenia(LocalDate.of(1964,1,1))
+				.rok_zalozenia(LocalDate.of(1964, 1, 1))
 				.liga(Ekstraklasa)
+				.trofea(new ArrayList<>())
 				.build();
 
 
 		Klub GornikZabrze = Klub.builder()
 				.nazwa_klubu("Gornik Zabrze")
-				.rok_zalozenia(LocalDate.of(1948,1,1))
+				.rok_zalozenia(LocalDate.of(1948, 1, 1))
 				.liga(Ekstraklasa)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub RakowCzestochowa = Klub.builder()
 				.nazwa_klubu("Rakow Czestochowa")
-				.rok_zalozenia(LocalDate.of(1921,1,1))
+				.rok_zalozenia(LocalDate.of(1921, 1, 1))
 				.liga(Ekstraklasa)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub ZaglebieLubin = Klub.builder()
 				.nazwa_klubu("KGHM Zaglebie Lubin")
-				.rok_zalozenia(LocalDate.of(1946,1,1))
+				.rok_zalozenia(LocalDate.of(1946, 1, 1))
 				.liga(Ekstraklasa)
+				.trofea(new ArrayList<>())
 				.build();
 
 
 		Klub SlaskWroclaw = Klub.builder()
 				.nazwa_klubu("Slask Wroclaw")
-				.rok_zalozenia(LocalDate.of(1947,1,1))
+				.rok_zalozenia(LocalDate.of(1947, 1, 1))
 				.liga(Ekstraklasa)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub RadomiakRadom = Klub.builder()
 				.nazwa_klubu("Radomiak Radom")
-				.rok_zalozenia(LocalDate.of(1910,1,1))
+				.rok_zalozenia(LocalDate.of(1910, 1, 1))
 				.liga(Ekstraklasa)
+				.trofea(new ArrayList<>())
 				.build();
 
 		klubRepository.save(LegiaWarszawa);
@@ -112,51 +126,59 @@ public class ProjektInzynierskiApplication implements CommandLineRunner {
 		//Kluby I Ligii
 		Klub RuchChorzow = Klub.builder()
 				.nazwa_klubu("Ruch Chorzow")
-				.rok_zalozenia(LocalDate.of(1920,1,1))
+				.rok_zalozenia(LocalDate.of(1920, 1, 1))
 				.liga(ILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub ZniczPruszkow = Klub.builder()
 				.nazwa_klubu("Znicz Pruszkow")
-				.rok_zalozenia(LocalDate.of(1922,1,1))
+				.rok_zalozenia(LocalDate.of(1922, 1, 1))
 				.liga(ILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub PogonSiedlce = Klub.builder()
 				.nazwa_klubu("Pogon Siedlce")
-				.rok_zalozenia(LocalDate.of(1944,1,1))
+				.rok_zalozenia(LocalDate.of(1944, 1, 1))
 				.liga(ILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 
 		Klub GKSTychy = Klub.builder()
 				.nazwa_klubu("GKS Tychy")
-				.rok_zalozenia(LocalDate.of(1971,1,1))
+				.rok_zalozenia(LocalDate.of(1971, 1, 1))
 				.liga(ILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub BrukBetTermalica = Klub.builder()
 				.nazwa_klubu("Bruk-Bet Termalica")
-				.rok_zalozenia(LocalDate.of(1922,1,1))
+				.rok_zalozenia(LocalDate.of(1922, 1, 1))
 				.liga(ILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub ArkaGdynia = Klub.builder()
 				.nazwa_klubu("Arka Gdynia")
-				.rok_zalozenia(LocalDate.of(1929,1,1))
+				.rok_zalozenia(LocalDate.of(1929, 1, 1))
 				.liga(ILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub MiedzLegnica = Klub.builder()
 				.nazwa_klubu("Miedz Legnica")
-				.rok_zalozenia(LocalDate.of(1971,1,1))
+				.rok_zalozenia(LocalDate.of(1971, 1, 1))
 				.liga(ILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub GornikLeczna = Klub.builder()
 				.nazwa_klubu("Gornik Leczna")
-				.rok_zalozenia(LocalDate.of(1979,1,1))
+				.rok_zalozenia(LocalDate.of(1979, 1, 1))
 				.liga(ILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 
@@ -172,50 +194,58 @@ public class ProjektInzynierskiApplication implements CommandLineRunner {
 		//Kluby z II Ligii
 		Klub RekordBB = Klub.builder()
 				.nazwa_klubu("Rekord Bielsko-Biala")
-				.rok_zalozenia(LocalDate.of(1994,1,1))
+				.rok_zalozenia(LocalDate.of(1994, 1, 1))
 				.liga(IILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub PodbeskidzieBB = Klub.builder()
 				.nazwa_klubu("TS Podbeskidzie Bielsko-Biala")
-				.rok_zalozenia(LocalDate.of(1995,1,1))
+				.rok_zalozenia(LocalDate.of(1995, 1, 1))
 				.liga(IILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub KKSKalisz = Klub.builder()
 				.nazwa_klubu("KKS 1925 Kalisz")
-				.rok_zalozenia(LocalDate.of(1925,1,1))
+				.rok_zalozenia(LocalDate.of(1925, 1, 1))
 				.liga(IILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub ZaglebieSosnowiec = Klub.builder()
 				.nazwa_klubu("Zaglebie Sosnowiec")
-				.rok_zalozenia(LocalDate.of(1918,1,1))
+				.rok_zalozenia(LocalDate.of(1918, 1, 1))
 				.liga(IILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub KSWieczystaKrakow = Klub.builder()
 				.nazwa_klubu("KS Wieczysta Krakow")
-				.rok_zalozenia(LocalDate.of(1942,1,1))
+				.rok_zalozenia(LocalDate.of(1942, 1, 1))
 				.liga(IILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub ResoviaRzeszow = Klub.builder()
 				.nazwa_klubu("Resovia Rzeszow")
-				.rok_zalozenia(LocalDate.of(1910,1,1))
+				.rok_zalozenia(LocalDate.of(1910, 1, 1))
 				.liga(IILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub PoloniaBytom = Klub.builder()
 				.nazwa_klubu("Polonia Bytom")
-				.rok_zalozenia(LocalDate.of(1945,1,1))
+				.rok_zalozenia(LocalDate.of(1945, 1, 1))
 				.liga(IILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		Klub OlimpiaGrudziac = Klub.builder()
 				.nazwa_klubu("Olimpia Grudziac")
-				.rok_zalozenia(LocalDate.of(1923,6,30))
+				.rok_zalozenia(LocalDate.of(1923, 6, 30))
 				.liga(IILiga)
+				.trofea(new ArrayList<>())
 				.build();
 
 		klubRepository.save(RekordBB);
@@ -227,11 +257,101 @@ public class ProjektInzynierskiApplication implements CommandLineRunner {
 		klubRepository.save(PoloniaBytom);
 		klubRepository.save(OlimpiaGrudziac);
 
+		//Legia
+		Trofeum PucharPolski2023 = Trofeum.builder()
+				.data_zdobycia(LocalDate.of(2023, 5, 2))
+				.nazwa("Puchar Polski 2023")
+				.klub(LegiaWarszawa)
+				.build();
+
+		Trofeum MistrzPolski2021 = Trofeum.builder()
+				.data_zdobycia(LocalDate.of(2021, 5, 16))
+				.nazwa("Mistrz Polski 2021")
+				.klub(LegiaWarszawa)
+				.build();
+
+		Trofeum MistrzPolski2020 = Trofeum.builder()
+				.data_zdobycia(LocalDate.of(2020, 7, 19))
+				.nazwa("Mistrz Polski 2020")
+				.klub(LegiaWarszawa)
+				.build();
+
+		Trofeum MistrzPolski2018 = Trofeum.builder()
+				.data_zdobycia(LocalDate.of(2018, 5, 20))
+				.nazwa("Mistrz Polski 2018")
+				.klub(LegiaWarszawa)
+				.build();
+
+		LegiaWarszawa.getTrofea().add(MistrzPolski2018);
+		LegiaWarszawa.getTrofea().add(MistrzPolski2020);
+		LegiaWarszawa.getTrofea().add(MistrzPolski2021);
+		LegiaWarszawa.getTrofea().add(PucharPolski2023);
+
+		trofeumRepository.save(MistrzPolski2018);
+		trofeumRepository.save(MistrzPolski2020);
+		trofeumRepository.save(MistrzPolski2021);
+		trofeumRepository.save(PucharPolski2023);
+
+
+		//Górnik
+		Trofeum MistrzPolski1988= Trofeum.builder()
+				.data_zdobycia(LocalDate.of(1988, 6, 18))
+				.nazwa("Mistrz Polski 1988")
+				.klub(GornikZabrze)
+				.build();
+
+		Trofeum MistrzPolski1987 = Trofeum.builder()
+				.data_zdobycia(LocalDate.of(1987, 6, 21))
+				.nazwa("Mistrz Polski 1987")
+				.klub(GornikZabrze)
+				.build();
+
+		Trofeum MistrzPolski1986 = Trofeum.builder()
+				.data_zdobycia(LocalDate.of(1986, 4, 27))
+				.nazwa("Mistrz Polski 1986")
+				.klub(GornikZabrze)
+				.build();
+
+		Trofeum MistrzPolski1985 = Trofeum.builder()
+				.data_zdobycia(LocalDate.of(1985, 6, 23))
+				.nazwa("Mistrz Polski 1985")
+				.klub(GornikZabrze)
+				.build();
+
+
+		GornikZabrze.getTrofea().add(MistrzPolski1985);
+		GornikZabrze.getTrofea().add(MistrzPolski1986);
+		GornikZabrze.getTrofea().add(MistrzPolski1987);
+		GornikZabrze.getTrofea().add(MistrzPolski1988);
+
+		trofeumRepository.save(MistrzPolski1985);
+		trofeumRepository.save(MistrzPolski1986);
+		trofeumRepository.save(MistrzPolski1987);
+		trofeumRepository.save(MistrzPolski1988);
+
+
+
+		System.out.println("\n");
+		List<Trofeum> trofea = LegiaWarszawa.getTrofea();
+		System.out.println("Trofea Legii Warszawa");
+		for (Trofeum trofeum : trofea) {
+			System.out.println("- " + trofeum.getNazwa() + " (Zdobyte: " + trofeum.getData_zdobycia() + ")");
+		}
+
+		System.out.println("");
+		List<Trofeum> trofeaG = GornikZabrze.getTrofea();
+		System.out.println("Trofea Gornika Zabrze");
+		for (Trofeum trofeum : trofeaG) {
+			System.out.println("- " + trofeum.getNazwa() + " (Zdobyte: " + trofeum.getData_zdobycia() + ")");
+		}
+
+		System.out.println("\n");
+
 
 		List<Klub> kluby = klubRepository.findAll();
 		System.out.println("Lista klubow:");
 		for (Klub klub : kluby) {
-			System.out.println("Klub: " + klub.getNazwa_klubu() + ", Rok zalozenia: " + klub.getRok_zalozenia()+ ", Liga: " + klub.getLiga().getNazwa_Ligi());
+			System.out.println("Klub: " + klub.getNazwa_klubu() + ", Rok zalozenia: " + klub.getRok_zalozenia() + ", Liga: " + klub.getLiga().getNazwa_Ligi());
 		}
 	}
 }
--- Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/models/Trofeum.java
+++ Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/models/Trofeum.java
@@ -3,14 +3,12 @@ package dev.projekt_inzynierski.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 
+@Builder
 @Getter
 @Setter
 @AllArgsConstructor
