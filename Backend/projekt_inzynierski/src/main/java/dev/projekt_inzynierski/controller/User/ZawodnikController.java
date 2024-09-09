ff241b7253bff714b95e98e5d1856e45506af075
qantek_ <s25610@pjwstk.edu.pl>
2024-09-09 20:11:36 +0200

    Dodanie końcówki do znalezienia zawodnika przez id w ZawodnikController
    20:10

--- Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/controller/User/ZawodnikController.java
+++ Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/controller/User/ZawodnikController.java
@@ -1,13 +1,29 @@
 package dev.projekt_inzynierski.controller.User;
 
+import dev.projekt_inzynierski.models.users.Zawodnik;
+import dev.projekt_inzynierski.repository.User.ZawodnikRepository;
 import dev.projekt_inzynierski.service.User.ZawodnikService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class ZawodnikController {
     private final ZawodnikService zawodnikService;
+    private final ZawodnikRepository zawodnikRepository;
 
-    public ZawodnikController(ZawodnikService zawodnikService) {
+    public ZawodnikController(ZawodnikService zawodnikService,ZawodnikRepository zawodnikRepository) {
         this.zawodnikService = zawodnikService;
+        this.zawodnikRepository=zawodnikRepository;
+    }
+
+    @GetMapping("/getZawodnik/{id_Uzytkownik}")
+    public ResponseEntity<Zawodnik> getZawodnikById(@PathVariable Long id_Uzytkownik) {
+        Optional<Zawodnik> zawodnik = zawodnikRepository.findById(id_Uzytkownik);
+        return zawodnik.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
