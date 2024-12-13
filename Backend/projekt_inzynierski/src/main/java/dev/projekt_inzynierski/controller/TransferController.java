585d8f57b1f7950fb1090bf02ea1b3793529587e
qantek_ <s25610@pjwstk.edu.pl>
2024-12-13 19:16:10 +0100

    Dodalem endpointy w TransferController obslugujacy metody z TransferService
    19;16

--- Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/controller/TransferController.java
+++ Backend/projekt_inzynierski/src/main/java/dev/projekt_inzynierski/controller/TransferController.java
@@ -4,11 +4,9 @@ import dev.projekt_inzynierski.DTO.TransferDTO;
 import dev.projekt_inzynierski.DTO.TrofeumDTO;
 import dev.projekt_inzynierski.service.TransferService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -24,4 +22,51 @@ public class TransferController {
         }
         return ResponseEntity.ok(transfery);
     }
+    @PostMapping("/sendTransfer")
+    public ResponseEntity<String> sendTransfer(
+            @RequestParam("kwota") int kwota,
+            @RequestParam("id_zawodnik") long idZawodnik,
+            @RequestParam("id_klubOd") long idKlubOd,
+            @RequestParam("id_klubDo") long idKlubDo) {
+        try {
+            LocalDate dataTransferu = LocalDate.now();
+            transferService.sendTransfer(dataTransferu, kwota, idZawodnik, idKlubOd, idKlubDo);
+            return ResponseEntity.ok("Transfer zostal wyslany z powodzeniem");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500)
+                    .body("wystapil blad przy transferze " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/odrzuc/{id}")
+    public ResponseEntity<String> odrzucTransfer(@PathVariable long id) {
+        try {
+            transferService.odrzucTransfer(id);
+            return ResponseEntity.ok("Udalo sie odrzucic transfer");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("wystapil blad przy odrzucaniu transferu " + e.getMessage());
+        }
+    }
+
+
+    @PostMapping("/zaakceptuj")
+    public ResponseEntity<String> zaakceptujTransfer(
+            @RequestParam("id_transfer") long idTransfer,
+            @RequestParam("id_uzytkownik") long idUzytkownik,
+            @RequestParam("id_klubOd") long idKlubOd,
+            @RequestParam("id_klubDo") long idKlubDo) {
+        try {
+            LocalDate dataDo = LocalDate.now(); // Zamiana String na LocalDate
+            transferService.zaakceptujTransfer(idTransfer, dataDo, idUzytkownik, idKlubOd, idKlubDo);
+            return ResponseEntity.ok("Transfer zostal zaakceptowany");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Wystapil blad przy akceptacji transferu " + e.getMessage());
+        }
+    }
+
 }
+
+
