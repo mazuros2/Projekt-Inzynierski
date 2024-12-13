bd7c04207fa6d9000ccfbe5aaff93ea2308893e4
Mazuros2 <s24440@pjwstk.edu.pl>
2024-12-13 10:45:30 +0100

    naprawiłem wyświetlanie ligii i klubów z danej ligii - 10:45

--- Frontend/profrontend/src/components/Liga.js
+++ Frontend/profrontend/src/components/Liga.js
@@ -1,26 +1,81 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Liga = () => {
   const [liga, setLiga] = useState([]);
+  const navigate = useNavigate();
+  const [showSettings, setShowSettings] = useState(false);
 
-  useEffect(() => {
-    const authHeader = 'Basic ' + btoa('admin:admin');
-    axios.get('http://localhost:8080/ligii', {
-      headers: {
-        'Authorization': authHeader, 
+
+    useEffect(() => {
+      const token = sessionStorage.getItem("token");
+      if (!token) {
+        console.error("Brak tokena. Przekierowanie do logowania.");
+        navigate("/logowanie");
+        return;
       }
-    })
-    .then(response => {
+
+      axios
+      .get("http://localhost:8080/ligii", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
         setLiga(response.data);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-  }, []);
+      })
+      .catch((error) => {
+        console.error("Error fetching clubs:", error);
+      });
+    }, [navigate]);
+
+    const toggleSettings = () => {
+      setShowSettings(!showSettings);
+    };
+    const goToUserProfile = () => {
+      navigate('/user-profile');
+    };
 
   return (
     <div>
+      <div className="navbar">
+            <Link to="/">
+               <img
+                src="https://lh3.googleusercontent.com/proxy/4C4zlh5y6xvZC7MWNsG_99nE1x8yqQnSczaCD2cUy4xlvPOQFcm5vLMoEhrcczwjBcfADm4La8Li__oU9Gzy1Whmwpj1U0BvwG6FlMpj6y7cQuI4IfftojBNTeKQocivQu7lbKfiKvXW30jdeizyGN6AHdIUSpc7mWw1"
+                alt="Logo"
+                  className="navbar-logo"
+                />
+            </Link>
+              <h1 className="navbar-title"></h1>          
+                <div className="icons-container">
+                  <img
+                    src="https://icons.veryicon.com/png/o/miscellaneous/iview30-ios-style/ios-menu-4.png"
+                    alt="Ustawienia"
+                    className="settings-icon"
+                    onClick={toggleSettings}
+                  />
+                  <img
+                    src="https://www.pikpng.com/pngl/b/259-2599075_gear-user-account-person-configure-control-comments-security.png"
+                      alt="Użytkownik"
+                      className="user-icon"
+                      onClick={goToUserProfile}
+                    />
+            {showSettings && (
+                  <div className="settings-menu">
+                    <ul>
+                      <li onClick={() => navigate("/ligii")}>Ligii</li>
+                      <li>Kluby</li>
+                      <li>Zawodnicy</li>
+                      <li>Trenerzy</li>
+                      <li>Lista obserwowanych</li>
+                    </ul>
+                  </div>
+                  )}
+              </div>
+            </div>
+
       <h1>Wszystkie ligii</h1>
       {liga.length === 0 ? (
         <p>Brak danych</p>
--- Frontend/profrontend/src/components/LigaKluby.js
+++ Frontend/profrontend/src/components/LigaKluby.js
@@ -43,9 +43,10 @@
 // export default LigaKluby;
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams, useNavigate } from 'react-router-dom'; 
+import { useParams, useNavigate, Link } from 'react-router-dom'; 
 import '../cssFolder/Navbar.css';
 import '../cssFolder/LigaKluby.css'; 
+
 
 
 const LigaKluby = () => {
@@ -63,55 +64,65 @@ const LigaKluby = () => {
   };
 
   useEffect(() => {
-    const authHeader = 'Basic ' + btoa('admin:admin');
-    axios.get(`http://localhost:8080/${ligaId}/kluby`, { 
+    const token = sessionStorage.getItem("token");
+    if (!token) {
+      console.error("Brak tokena. Przekierowanie do logowania.");
+      navigate("/logowanie");
+      return;
+    }
+    
+    axios.get(`http://localhost:8080/${ligaId}/kluby`, {
       headers: {
-        'Authorization': authHeader,
-      }
+        Authorization: `Bearer ${token}`,
+      },
     })
-    .then(response => {
+    .then((response) => {
       setKluby(response.data);
     })
-    .catch(error => {
-      console.error('Error fetching data:', error);
+    .catch((error) => {
+      console.error("Error fetching clubs:", error);
     });
   }, [ligaId]);
 
   return (
     <div className="kluby-container">
-      {/* Pasek nawigacyjny */}
       <div className="navbar">
-        <img 
-          src="https://lh3.googleusercontent.com/proxy/4C4zlh5y6xvZC7MWNsG_99nE1x8yqQnSczaCD2cUy4xlvPOQFcm5vLMoEhrcczwjBcfADm4La8Li__oU9Gzy1Whmwpj1U0BvwG6FlMpj6y7cQuI4IfftojBNTeKQocivQu7lbKfiKvXW30jdeizyGN6AHdIUSpc7mWw1" 
-          alt="Logo" 
-          className="navbar-logo" 
-        />
-        <h1 className="navbar-title"></h1>
-        <div className="icons-container">
-          <img
-            src="https://www.pikpng.com/pngl/b/112-1121340_settings-logo-png-white-png-download-setting-icon.png"
-            alt="Ustawienia"
-            className="settings-icon"
-            onClick={toggleSettings}
-          />
-          <img
-            src="https://www.pikpng.com/pngl/b/259-2599075_gear-user-account-person-configure-control-comments-security.png"
-            alt="Użytkownik"
-            className="user-icon"
-            onClick={goToUserProfile}
-          />
-          {showSettings && (
-            <div className="settings-menu">
-              <ul>
-                <li>Opcja 1</li>
-                <li>Opcja 2</li>
-                <li>Opcja 3</li>
-              </ul>
+          <Link to="/">
+            <img
+              src="https://lh3.googleusercontent.com/proxy/4C4zlh5y6xvZC7MWNsG_99nE1x8yqQnSczaCD2cUy4xlvPOQFcm5vLMoEhrcczwjBcfADm4La8Li__oU9Gzy1Whmwpj1U0BvwG6FlMpj6y7cQuI4IfftojBNTeKQocivQu7lbKfiKvXW30jdeizyGN6AHdIUSpc7mWw1"
+              alt="Logo"
+              className="navbar-logo"
+            />
+          </Link>
+            <h1 className="navbar-title"></h1>
+            
+            <div className="icons-container">
+              <img
+                src="https://icons.veryicon.com/png/o/miscellaneous/iview30-ios-style/ios-menu-4.png"
+                alt="Ustawienia"
+                className="settings-icon"
+                onClick={toggleSettings}
+              />
+              <img
+                src="https://www.pikpng.com/pngl/b/259-2599075_gear-user-account-person-configure-control-comments-security.png"
+                alt="Użytkownik"
+                className="user-icon"
+                onClick={goToUserProfile}
+              />
+              {showSettings && (
+                <div className="settings-menu">
+                  <ul>
+                    <li onClick={() => navigate("/ligii")}>Ligii</li>
+                    <li>Kluby</li>
+                    <li>Zawodnicy</li>
+                    <li>Trenerzy</li>
+                    <li>Lista obserwowanych</li>
+                  </ul>
+                </div>
+                )}
             </div>
-          )}
         </div>
-      </div>
-      
+
       {/* Zawartość strony */}
       <h1>Kluby w Lidze {ligaId}</h1>
       {kluby.length === 0 ? (
