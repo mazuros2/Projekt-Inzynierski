fe59bbadb4e8fe5532bbaf25897b9846ecd2ec18
qantek_ <s25610@pjwstk.edu.pl>
2025-01-04 19:57:05 +0100

    Dodalem ze dla roli Menadzera automatycznie pobierana jest wartosc id jego klubu, menadzera nie moze teraz wpisac jakiegos losowego idKlubuDo do ktorego chcialby wyslac transfer.

--- Frontend/profrontend/src/components/Transfer.js
+++ Frontend/profrontend/src/components/Transfer.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import jwtDecode from "jwt-decode"; 
 import { Link, useNavigate, useParams } from "react-router-dom";
 import '../cssFolder/Navbar.css';
 
 const Transfer = () => {
-  const { id } = useParams(); // Pobierz ID zawodnika z URL
+  const { id } = useParams();
   const [kwota, setKwota] = useState("");
-  const [idKlubOd, setIdKlubOd] = useState(null); // Automatycznie pobrany klub
+  const [idKlubOd, setIdKlubOd] = useState(null);
   const [idKlubDo, setIdKlubDo] = useState("");
   const [error, setError] = useState("");
+  const [role, setRole] = useState(null); // Przechowywanie roli użytkownika
+  const [userId, setUserId] = useState(null); // Przechowywanie ID użytkownika z tokena
   const navigate = useNavigate();
   const [showSettings, setShowSettings] = useState(false);
 
@@ -21,29 +24,43 @@ const Transfer = () => {
   };
 
   const handleLogout = () => {
-    localStorage.removeItem('token'); 
-    sessionStorage.removeItem('token'); 
-    navigate('/logowanie'); 
+    localStorage.removeItem('token');
+    sessionStorage.removeItem('token');
+    navigate('/logowanie');
   };
 
-
+  const getUserInfo = () => {
+    const token = sessionStorage.getItem("token");
+    if (!token) return null;
+    try {
+      const decodedToken = jwtDecode(token);
+      console.log("Decoded token:", decodedToken); // Debugowanie tokena
+      setRole(decodedToken.role || null);
+      setUserId(decodedToken.userId || null);
+    } catch (error) {
+      console.error("Błąd dekodowania tokena:", error);
+      setRole(null);
+      setUserId(null);
+    }
+  };
 
   useEffect(() => {
-    const fetchKlubId = async () => {
-      try {
-        const token = sessionStorage.getItem("token");
-        if (!token) {
-          setError("Brak tokena. Zaloguj się ponownie.");
-          return;
-        }
+    const fetchRoleAndKlubData = async () => {
+      getUserInfo(); // Pobierz dane użytkownika z tokena
 
+      const token = sessionStorage.getItem("token");
+      if (!token) {
+        setError("Brak tokena. Zaloguj się ponownie.");
+        return;
+      }
+
+      try {
+        // Pobierz ID klubu "od"
         const response = await axios.get(`http://localhost:8080/findKlubIdByZawodnik/${id}`, {
           headers: {
             Authorization: `Bearer ${token}`
           }
         });
-
-        console.log(response.data); // Sprawdź, co zwraca backend
 
         if (response.data && response.data.id) {
           setIdKlubOd(response.data.id);
@@ -51,13 +68,39 @@ const Transfer = () => {
           setError("Nie znaleziono klubu dla tego zawodnika.");
         }
       } catch (error) {
-        console.error("Błąd przy pobieraniu ID klubu zawodnika:", error);
-        setError("Wystąpił problem podczas pobierania danych klubu.");
+        console.error("Błąd przy pobieraniu danych:", error);
+        setError("Wystąpił problem podczas pobierania danych.");
       }
     };
 
-    fetchKlubId();
+    fetchRoleAndKlubData();
   }, [id]);
+
+  useEffect(() => {
+    const fetchIdKlubuMenadzera = async () => {
+      if (role === "ROLE_MENADZER_KLUBU" && userId) {
+        try {
+          const token = sessionStorage.getItem("token");
+          const response = await axios.get(`http://localhost:8080/getIdKlubuMenadzera/${userId}`, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+
+          if (response.data) {
+            setIdKlubDo(response.data);
+          } else {
+            setError("Nie znaleziono klubu dla menedżera.");
+          }
+        } catch (error) {
+          console.error("Błąd przy pobieraniu ID klubu menedżera:", error);
+          setError("Wystąpił problem podczas pobierania danych klubu.");
+        }
+      }
+    };
+
+    fetchIdKlubuMenadzera();
+  }, [role, userId]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -84,7 +127,7 @@ const Transfer = () => {
       })
       .then(() => {
         alert("Transfer został wysłany z powodzeniem");
-        navigate(-1); // Wróć do poprzedniej strony
+        navigate(-1);
       })
       .catch((error) => {
         console.error("Błąd przy wysyłaniu transferu:", error);
@@ -94,8 +137,7 @@ const Transfer = () => {
 
   return (
     <div>
-    
-    <div className="navbar">
+      <div className="navbar">
         <Link to="/">
           <img
             src="https://lh3.googleusercontent.com/proxy/4C4zlh5y6xvZC7MWNsG_99nE1x8yqQnSczaCD2cUy4xlvPOQFcm5vLMoEhrcczwjBcfADm4La8Li__oU9Gzy1Whmwpj1U0BvwG6FlMpj6y7cQuI4IfftojBNTeKQocivQu7lbKfiKvXW30jdeizyGN6AHdIUSpc7mWw1"
@@ -103,69 +145,68 @@ const Transfer = () => {
             className="navbar-logo"
           />
         </Link>
-          <h1 className="navbar-title"></h1>
-          
-          <div className="icons-container">
-            <img
-              src="https://icons.veryicon.com/png/o/miscellaneous/iview30-ios-style/ios-menu-4.png"
-              alt="Ustawienia"
-              className="settings-icon"
-              onClick={toggleSettings}
-            />
-            <img
-              src="https://www.pikpng.com/pngl/b/259-2599075_gear-user-account-person-configure-control-comments-security.png"
-              alt="Użytkownik"
-              className="user-icon"
-              onClick={goToUserProfile}
-            />
-            {showSettings && (
-              <div className="settings-menu">
-                <ul>
-                  <li onClick={() => navigate("/ligii")}>Ligii</li>
-                  <li onClick={() => navigate('/kluby')}>Kluby</li>
-                  <li onClick={() => navigate('/zawodnicy')}>Zawodnicy</li>
-                  <li onClick={() => navigate('/trenerzy')}>Trenerzy</li>
-                  <li onClick={() => navigate("/listaObserwowanych")}>Lista obserwowanych</li>
-                  <li onClick={handleLogout}>Wyloguj</li>
-                </ul>
-              </div>
-              )}
-          </div>
+        <h1 className="navbar-title"></h1>
+        <div className="icons-container">
+          <img
+            src="https://icons.veryicon.com/png/o/miscellaneous/iview30-ios-style/ios-menu-4.png"
+            alt="Ustawienia"
+            className="settings-icon"
+            onClick={toggleSettings}
+          />
+          <img
+            src="https://www.pikpng.com/pngl/b/259-2599075_gear-user-account-person-configure-control-comments-security.png"
+            alt="Użytkownik"
+            className="user-icon"
+            onClick={goToUserProfile}
+          />
+          {showSettings && (
+            <div className="settings-menu">
+              <ul>
+                <li onClick={() => navigate("/ligii")}>Ligii</li>
+                <li onClick={() => navigate('/kluby')}>Kluby</li>
+                <li onClick={() => navigate('/zawodnicy')}>Zawodnicy</li>
+                <li onClick={() => navigate('/trenerzy')}>Trenerzy</li>
+                <li onClick={() => navigate("/listaObserwowanych")}>Lista obserwowanych</li>
+                <li onClick={handleLogout}>Wyloguj</li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
 
-    
-    <div>
-      <h1>Wysłanie transferu</h1>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Kwota: </label>
-          <input
-            type="number"
-            value={kwota}
-            onChange={(e) => setKwota(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Klub od (ID): </label>
-          <span>{idKlubOd || "Ładowanie..."}</span>
-        </div>
-        <div>
-          <label>Klub do (ID): </label>
-          <input
-            type="number"
-            value={idKlubDo}
-            onChange={(e) => setIdKlubDo(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" disabled={!idKlubOd}>
-          Wyślij transfer
-        </button>
-      </form>
-      <button onClick={() => navigate(-1)}>Anuluj</button>
-    </div>
+      <div>
+        <h1>Wysłanie transferu</h1>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Kwota: </label>
+            <input
+              type="number"
+              value={kwota}
+              onChange={(e) => setKwota(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Klub od (ID): </label>
+            <span>{idKlubOd || "Ładowanie..."}</span>
+          </div>
+          <div>
+            <label>Klub do (ID): </label>
+            <input
+              type="number"
+              value={idKlubDo}
+              onChange={(e) => setIdKlubDo(e.target.value)}
+              readOnly={role === "ROLE_MENADZER_KLUBU"} // Automatyczne ustawianie dla menedżera
+              required
+            />
+          </div>
+          <button type="submit" disabled={!idKlubOd}>
+            Wyślij transfer
+          </button>
+        </form>
+        <button onClick={() => navigate(-1)}>Anuluj</button>
+      </div>
     </div>
   );
 };
